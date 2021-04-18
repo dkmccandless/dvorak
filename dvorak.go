c9c57e290477b8249eed3437d8706e806e3beb53
Dan McCandless <dkmccandless@gmail.com>
2021-04-18 16:31:30 +0800

    parse Template:Card source code

--- dvorak.go
+++ dvorak.go
@@ -1,21 +1,35 @@
 // Package dvorak parses source code templates used by the Dvorak game wiki.
 package dvorak
 
+import (
+	"encoding/hex"
+	"errors"
+	"image/color"
+	"strings"
+)
+
 // Card is a Dvorak card.
 type Card struct {
 	// Title is the card's title.
 	Title string
 
+	// LongTitle indicates that Title is too long to fit the standard header.
+	// If not empty, it expands the header to fit Title and Type.
+	LongTitle string
+
 	// Text is the card's rule text.
 	Text string
+
+	// LongText indicates that Text is too long to fit the standard text area.
+	// If not empty, it reduces the Text and FlavorText font size.
+	LongText string
 
 	// Type is the card's type, usually "Action" or "Thing".
 	Type string
 
-	// BGColor is the color of the card header background, as a hex value.
-	// If omitted, the default values according to card type are
-	// 600 for "Action", 006 for "Thing", or else 666.
-	BGColor string
+	// BGColor is the color of the card header background.
+	// If omitted, ParseCard sets a default value according to the card type.
+	BGColor *color.RGBA
 
 	// CornerValue is an optional value to print in the card's top right corner.
 	CornerValue string
@@ -23,30 +37,187 @@ type Card struct {
 	// Image is the filename of an image for the card.
 	Image string
 
-	// ImgBack is the color to be shown behind the card image, as a hex value.
-	ImgBack string
+	// ImgBack is the optional color to be shown behind the card image.
+	ImgBack *color.RGBA
 
-	// FlavorText is the card's flavor text. If not empty, this is shown under
-	// the rule text, separated by a horizontal line.
+	// FlavorText is the card's flavor text. If not empty, this is displayed
+	// under the rule text, separated by a horizontal line.
 	FlavorText string
 
-	// Creator is the player who created the card. If not empty, this value is
+	// Creator is the player who created the card. If not empty, this is
 	// displayed at the bottom of the card.
 	Creator string
-
-	// LongTitle, LongText, and MiniCard are optional string flags that modify
-	// the layout of the card if they are not the empty string.
-
-	// LongTitle indicates the title is too long to fit the standard header.
-	// If not empty, it expands the header to fit the title and type.
-	LongTitle string
-
-	// LongText indicates the text is too long to fit the standard text area.
-	// If not empty, it reduces the text and flavor text font size.
-	LongText string
 
 	// MiniCard indicates that the card is smaller than standard size,
 	// e.g. for display in example texts.
 	// If not empty, it reduces the card's size.
 	MiniCard string
+}
+
+// ParseCard parses a single instance of Template:Card source code.
+func ParseCard(s string) (*Card, error) {
+	// http://dvorakgame.co.uk/index.php/Template:Card
+	// https://meta.wikimedia.org/wiki/Help:Template
+
+	var errInvalid = errors.New("invalid syntax")
+
+	if !strings.HasPrefix(s, "{{") {
+		return nil, errInvalid
+	}
+	s = s[2:]
+
+	next := nextDelimiter(s)
+	if next == -1 {
+		return nil, errInvalid
+	}
+
+	name := strings.TrimSpace(s[:next])
+	if strings.HasPrefix(name, "Template:") ||
+		strings.HasPrefix(name, "template:") {
+		name = name[9:]
+	}
+	if name != "Card" && name != "card" {
+		return nil, errInvalid
+	}
+	s = s[next:]
+
+	c := &Card{}
+	for len(s) > 2 {
+		// Disallow nested templates by only accepting "|".
+		if !strings.HasPrefix(s, "|") {
+			return nil, errInvalid
+		}
+		s = s[1:]
+
+		next := nextDelimiter(s)
+		if next == -1 {
+			return nil, errInvalid
+		}
+
+		switch name, value := parseParameter(s[:next]); name {
+		case "title":
+			c.Title = value
+		case "longtitle":
+			c.LongTitle = value
+		case "text":
+			c.Text = value
+		case "longtext":
+			c.LongText = value
+		case "type":
+			c.Type = value
+		case "bgcolor":
+			c.BGColor = parseRGB(value)
+		case "cornervalue":
+			c.CornerValue = value
+		case "image":
+			c.Image = value
+		case "imgback":
+			c.ImgBack = parseRGB(value)
+		case "flavortext":
+			c.FlavorText = value
+		case "creator":
+			c.Creator = value
+		case "minicard":
+			c.MiniCard = value
+		}
+
+		s = s[next:]
+	}
+	if s != "}}" {
+		return nil, errInvalid
+	}
+
+	if c.BGColor == nil {
+		c.BGColor = defaultBGColor(c.Type)
+	}
+	return c, nil
+}
+
+// parseParameter parses a named template parameter.
+// Whitespace is trimmed from the returned strings.
+// If s does not contain "=", parseParameter returns "", "" instead.
+func parseParameter(s string) (name, value string) {
+	eq := strings.Index(s, "=")
+	if eq == -1 {
+		return
+	}
+	return strings.TrimSpace(s[:eq]), strings.TrimSpace(s[eq+1:])
+}
+
+// parseRGB parses a hex string and returns the corresponding color.
+// The string must be length 3 or 6 and contain only hexadecimal characters.
+// Otherwise, parseRGB returns nil.
+func parseRGB(s string) *color.RGBA {
+	if len(s) == 3 {
+		s = string([]byte{s[0], s[0], s[1], s[1], s[2], s[2]})
+	}
+	if len(s) != 6 {
+		return nil
+	}
+	rgb, err := hex.DecodeString(s)
+	if err != nil {
+		return nil
+	}
+	return &color.RGBA{rgb[0], rgb[1], rgb[2], 255}
+}
+
+// Default header background colors
+var (
+	actionRed = parseRGB("600")
+	thingBlue = parseRGB("006")
+	otherGray = parseRGB("666")
+)
+
+// defaultBGColor returns the default header background color for typ.
+func defaultBGColor(typ string) *color.RGBA {
+	switch typ {
+	case "Action":
+		return actionRed
+	case "Thing":
+		return thingBlue
+	default:
+		return otherGray
+	}
+}
+
+// nextDelimiter returns the index of the first delimiter character at the end
+// of the first field in s. This is the first instance of "|" not contained
+// within a matching pair of double brackets, or else the first "}}".
+func nextDelimiter(s string) int {
+	// minExists returns its smallest non-negative argument,
+	// or -1 if both arguments are negative.
+	minExists := func(x, y int) int {
+		switch {
+		case x < 0:
+			if y < 0 {
+				return -1
+			}
+			return y
+		case y < 0, x < y:
+			return x
+		default:
+			return y
+		}
+	}
+
+	lbr := strings.Index(s, "[[")
+	pipe := strings.Index(s, "|")
+	cl := strings.Index(s, "}}")
+
+	if minExists(lbr, pipe) == pipe || minExists(lbr, cl) == cl {
+		return minExists(pipe, cl)
+	}
+
+	// Left double bracket occurs first; find the next right double bracket.
+	rbroffset := strings.Index(s[lbr:], "]]")
+	if rbroffset == -1 {
+		return minExists(pipe, cl)
+	}
+
+	lenbr := lbr + rbroffset + 2
+	next := nextDelimiter(s[lenbr:])
+	if next == -1 {
+		return -1
+	}
+	return lenbr + next
 }
